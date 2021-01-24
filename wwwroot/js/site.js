51e885ac98b884cc02fc57a3e7a1f0a34d59ecfd
Michael Glimmerdahl <migl1900@student.miun.se>
2021-01-24 16:24:24 +0100

    Add project files.

---
+++ wwwroot/js/site.js
@@ -0,0 +1,14 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+"use strict"
+
+function changeColor(className) {
+    var header = document.querySelectorAll("h1");
+
+    for (var i = 0; i < header.length; i++) {
+        header[i].className = "";
+        header[i].classList.add("display-4", className);
+    }
+}
